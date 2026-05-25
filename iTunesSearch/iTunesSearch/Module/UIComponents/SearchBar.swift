3fcc0a629bd53bb9721879bfb68d2570b3f40465
Beatriz Ogioni <biaogioni@M4Pro-10Core-16GB-001.local>
2026-05-25 20:27:39 -0300

    refactor SearchView and improve viewmodel

---
+++ iTunesSearch/iTunesSearch/Module/UIComponents/SearchBar.swift
@@ -0,0 +1,39 @@
+import SwiftUI
+ 
+struct SearchBar: View {
+    @Binding var text: String
+    var focused: FocusState<Bool>.Binding
+    var placeholder: String = "Search"
+    var onSubmit: () -> Void
+ 
+    var body: some View {
+        HStack {
+            Image(symbol: .magnifyingglass)
+                .foregroundStyle(.element25)
+                .padding(.leading, 18)
+ 
+            TextField(placeholder, text: $text)
+                .textFieldStyle(.plain)
+                .focused(focused)
+                .font(.system(size: 16, weight: .medium))
+                .submitLabel(.search)
+                .foregroundStyle(.text01)
+                .onSubmit(onSubmit)
+ 
+            if !text.isEmpty {
+                Button {
+                    text = ""
+                } label: {
+                    Image(symbol: .xmarkCircleFill)
+                        .foregroundStyle(.element25)
+                }
+                .padding(.trailing, 18)
+            }
+        }
+        .padding(.vertical, 12)
+        .background(.element10)
+        .cornerRadius(12)
+        .padding(.horizontal, 10)
+    }
+}
+ 
